8e60ef8e26f33baddb9926b3d8ff4059583412ba
Phani Kumar <phani.engg@gmail.com>
2023-01-17 13:54:02 +0530

    db change

--- backend/database/database.py
+++ backend/database/database.py
@@ -2,9 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./interview_automation.db'
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./interview_automation.db'
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SQLALCHEMY_DATABASE_URL = 'postgresql://pvsngeqq:Y58UwShiPlB7---M91sSqIb-upL4dDoy@floppy.db.elephantsql.com/pvsngeqq'
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
